fc98ac5e7eabcf2dfe942cd9739f4f86f5c0d9a9
abhikjain360 <abhikjain360@gmail.com>
2020-06-21 18:13:35 +0530

    renderer class added

--- renderer.cpp
+++ renderer.cpp
@@ -13,3 +13,16 @@ bool gl_call_log(const char* func, const char* file, int line) {
 	}
 	return true;
 }
+
+void renderer::draw(const vertex_array& va, const index_buffer& ib, const shader& shader) const {
+	shader.bind();
+	//shader.set_uniform4f("u_color", r, 0.3f, 0.8f, 1.0f);
+	va.bind();
+	ib.bind();
+
+	GLCALL( glDrawElements(GL_TRIANGLES, ib.get_count(), GL_UNSIGNED_INT, nullptr) );
+}
+
+void renderer::clear() const {
+	GLCALL( glClear(GL_COLOR_BUFFER_BIT) );
+}
--- renderer.h
+++ renderer.h
@@ -2,11 +2,20 @@
 
 #include <GL/glew.h>
 
-#define ASSERT(x) if(!(x)) exit(-1);
+#include "vertex_array.h"
+#include "index_buffer.h"
+#include "shader.h"
 
+#define ASSERT(x) if(!(x)) exit(-1);
 #define GLCALL(x) gl_clear_error();\
 	x;\
 	ASSERT(gl_call_log(#x, __FILE__, __LINE__))
 
 void gl_clear_error();
 bool gl_call_log(const char* func, const char* file, int line);
+
+class renderer {
+  public:
+	void draw(const vertex_array& va, const index_buffer& ib, const shader& shader) const;
+	void clear() const;
+};
